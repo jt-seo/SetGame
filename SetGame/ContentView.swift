c62dd37dc83e1c4ebf10359c78c55e510e651a52
jt.seo <jt.seo@nexplayer.com>
2020-08-20 22:28:22 +0900

    Initial Commit

---
+++ SetGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SetGame
+//
+//  Created by JT3 on 2020/08/20.
+//  Copyright © 2020 JT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
